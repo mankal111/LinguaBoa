e7d10589e486e4681550701470dcdad8356b7eba
mankal111 <mankal111@hotmail.com>
2020-09-01 12:19:02 +0300

    added animals to words

--- src/words.js
+++ src/words.js
@@ -1,22 +1,27 @@
 export const symbols = {
     numbers: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
+    animals: ['🐻', '🐎', '🐘', '🐱', '🐦', '🐭']
 }
 
 export const words = {
     english: {
         code: 'en',
-        numbers: ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']
+        numbers: ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'],
+        animals: ['bear', 'horse', 'elephant', 'cat', 'bird', 'mouse'],
     },
     german: {
         code: 'de-DE',
-        numbers: ['null', 'eins', 'zwei', 'drei', 'vier', 'fünf', 'sechs', 'sieben', 'acht', 'neun', 'zehn'] 
+        numbers: ['null', 'eins', 'zwei', 'drei', 'vier', 'fünf', 'sechs', 'sieben', 'acht', 'neun', 'zehn'],
+        animals: ["Bär", "Pferd", "Elefant", "Katze", 'vogel', "Maus"],
     },
     albanian: {
         code: 'sq-AL',
-        numbers: ['zero', 'një', 'dy', 'tre', 'katër', 'pesë', 'gjashtë', 'shtatë', 'tetë', 'nëntë', 'dhjetë']
+        numbers: ['zero', 'një', 'dy', 'tre', 'katër', 'pesë', 'gjashtë', 'shtatë', 'tetë', 'nëntë', 'dhjetë'],
+        animals: ['ariu', 'kali', 'elefanti', 'mace', 'zog', 'miu'],
     },
     greek: {
         code: 'el-GR',
-        numbers: ['μηδέν', 'ένα', 'δύο', 'τρία', 'τέσσερα', 'πέντε', 'έξι', 'εφτά', 'οχτώ', 'εννιά', 'δέκα']
+        numbers: ['μηδέν', 'ένα', 'δύο', 'τρία', 'τέσσερα', 'πέντε', 'έξι', 'εφτά', 'οχτώ', 'εννιά', 'δέκα'],
+        animals: ['αρκούδα', 'άλογο', 'ελέφαντας', 'γάτα', 'πουλί', 'ποντίκι'],
     }
 }
