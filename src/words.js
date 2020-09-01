80e15f101b361b8d85c0b24018cd2ea90acdec46
mankal111 <mankal111@hotmail.com>
2020-09-01 12:06:41 +0300

    added words

---
+++ src/words.js
@@ -0,0 +1,22 @@
+export const symbols = {
+    numbers: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
+}
+
+export const words = {
+    english: {
+        code: 'en',
+        numbers: ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']
+    },
+    german: {
+        code: 'de-DE',
+        numbers: ['null', 'eins', 'zwei', 'drei', 'vier', 'fünf', 'sechs', 'sieben', 'acht', 'neun', 'zehn'] 
+    },
+    albanian: {
+        code: 'sq-AL',
+        numbers: ['zero', 'një', 'dy', 'tre', 'katër', 'pesë', 'gjashtë', 'shtatë', 'tetë', 'nëntë', 'dhjetë']
+    },
+    greek: {
+        code: 'el-GR',
+        numbers: ['μηδέν', 'ένα', 'δύο', 'τρία', 'τέσσερα', 'πέντε', 'έξι', 'εφτά', 'οχτώ', 'εννιά', 'δέκα']
+    }
+}
