be2f52aafe00d58525839ef5c47ac49e09f3f006
mankal111 <mankal111@hotmail.com>
2020-09-02 09:51:01 +0300

    Updates

---
+++ precache-manifest.660364570659022e8745d14e1d089f92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4b2d648ca9adfe4180faae3c4f1565b",
+    "url": "/linguaboa/index.html"
+  },
+  {
+    "revision": "1344f3b1a9ffdafc6dd7",
+    "url": "/linguaboa/static/css/main.48b274bd.chunk.css"
+  },
+  {
+    "revision": "68a5589f78ff9262c2c7",
+    "url": "/linguaboa/static/js/2.146c4326.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/linguaboa/static/js/2.146c4326.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1344f3b1a9ffdafc6dd7",
+    "url": "/linguaboa/static/js/main.93429525.chunk.js"
+  },
+  {
+    "revision": "5b6c2a6fb5d193b9a29d",
+    "url": "/linguaboa/static/js/runtime-main.cfe35ea1.js"
+  }
+]);
