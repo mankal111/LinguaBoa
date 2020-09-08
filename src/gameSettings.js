f8c2f907fc68e47de6893851f3fb846ba1553b02
mankal111 <mankal111@hotmail.com>
2020-09-08 10:19:31 +0300

    changed settings

--- src/gameSettings.js
+++ src/gameSettings.js
@@ -1,7 +1,7 @@
-export const boardSize = 12;
+export const boardSize = 15;
 export const snakeInitialSize = 3;
 export const snakeLengthIncrease = 3;
 export const scorePerFood = 10;
-export const initialSpeed = 2; //moves per second
-export const speedIncrement = 0.5;
+export const initialSpeed = 3; //moves per second
+export const speedIncrement = 0.2;
 export const limitSpeed = 5;
