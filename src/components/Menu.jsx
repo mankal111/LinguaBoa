52d2358e488f694e32ecf5f811c9fa64e37c72ba
mankal111 <mankal111@hotmail.com>
2021-03-22 19:12:29 +0200

    used styled-components in menu, included title

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,16 +1,54 @@
 import React from 'react';
+import styled from 'styled-components';
 import { words, symbols } from '../words';
+
+const Container = styled.div`
+  background-color: #0082B4;
+  border-radius: 5px;
+  box-shadow: 5px 5px 5px 1px black, inset 1px 1px 5px white;
+`
+
+const Title = styled.h1`
+  text-shadow: 5px 5px #004200;
+  width: 100%;
+  text-align: center;
+`
+
+const Options = styled.form`
+  display: flex;
+  flex-direction: column;
+  padding: 50px;
+  padding-top: 0;
+`
+
+const Dropdown = styled.label`
+  padding: 20px;
+`
+
+const PlayBtn = styled.input`
+  height: 100px;
+  width: 300px;
+  background-color: #63EC2E;
+  color: #004200;
+  font-family: "Reggae One";
+  font-size: 60px;
+  border: none;
+  cursor: pointer;
+  box-shadow: 2px 2px 2px 5px #004200, inset 1px 1px 3px white;
+  border-radius: 10px;
+`
 
 function Menu(props) {
   const languages = Object.keys(words);
   const subjects = Object.keys(symbols);
 
-  const { setLanguage, setSubject, play, language, subject} = props;
+  const { setLanguage, setSubject, play, language, subject } = props;
 
   return (
-    <div className="Menu">
-      <form>
-        <label htmlFor="language-select">
+    <Container>
+      <Title>LinguaBoa</Title>
+      <Options>
+        <Dropdown htmlFor="language-select">
           Language:
           <select
             id="language-select"
@@ -28,8 +66,8 @@ function Menu(props) {
               ))
             }
           </select>
-        </label>
-        <label htmlFor="subject-select">
+        </Dropdown>
+        <Dropdown htmlFor="subject-select">
           Subject:
           <select
             id="subject-select"
@@ -45,10 +83,10 @@ function Menu(props) {
               </option>
             ))}
           </select>
-        </label>
-        <input type="button" value="Play" onClick={play} />
-      </form>
-    </div>
+        </Dropdown>
+        <PlayBtn type="button" value="PLAY" onClick={play} />
+      </Options>
+    </Container>
   );
 }
 
