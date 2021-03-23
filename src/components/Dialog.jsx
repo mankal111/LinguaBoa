1a1db83b41dff41faa0c037648ce6615aaee0167
mankal111 <mankal111@hotmail.com>
2021-03-23 18:35:49 +0200

    used styled-components in dialog

--- src/components/Dialog.jsx
+++ src/components/Dialog.jsx
@@ -1,19 +1,52 @@
 import React from 'react';
+import styled from 'styled-components';
 import PropTypes from 'prop-types';
-import './Dialog.css';
+
+const Container = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #0082B4;
+  color: #004200;
+  border-radius: 10px;
+  box-shadow: 5px 5px 15px black, inset 1px 1px 3px white, inset -1px -1px 3px black;
+  padding: 20px;
+`
+const Message = styled.div``
+
+const ButtonsContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  margin: 10px;
+`
+
+const Button = styled.button`
+  height: 50px;
+  width: 250px;
+  background-color: #63EC2E;
+  color: #004200;
+  font-family: "Reggae One";
+  font-size: 20px;
+  border: none;
+  cursor: pointer;
+  box-shadow: 2px 2px 2px 5px #004200, inset 1px 1px 3px white;
+  border-radius: 10px;
+`
 
 function Dialog(props) {
   const { exit, restart } = props;
   return (
-    <div className="dialog">
-      <div className="dialog-message">
+    <Container>
+      <Message>
         Sometimes you win and sometimes you learn.
-      </div>
-      <div className="dialog-buttons">
-        <button type="button" onClick={restart}>Play again</button>
-        <button type="button" onClick={exit}>Back to main Menu</button>
-      </div>
-    </div>
+      </Message>
+      <ButtonsContainer>
+        <Button onClick={restart}>Play again</Button>
+        <Button onClick={exit}>Back to main Menu</Button>
+      </ButtonsContainer>
+    </Container>
   );
 }
 
